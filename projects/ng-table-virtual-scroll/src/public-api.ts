7c7c0c7b98e35bfbcef0a6c71357c96f602742d5
marko <mnikolic@lioneight.com>
2020-03-06 16:10:04 +0100

    Add only dist files

--- projects/ng-table-virtual-scroll/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of ng-fixed-size-table-virtual-scroll
- */
-
-export * from './lib/table-virtual-scroll.module';
-export * from './lib/table-item-size.directive';
-export * from './lib/fixed-size-table-virtual-scroll-strategy';
-export * from './lib/table-data-source';
